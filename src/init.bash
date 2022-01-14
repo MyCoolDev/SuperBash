c49904dc36777b645a1def26ade725f1dbef1eae
kfir <kfirbotnik@gmail.com>
2022-01-14 22:38:38 +0000

    Update init.bash

--- src/init.bash
+++ src/init.bash
@@ -1,5 +1,4 @@
 init() {
-  source $HOME/.config/bash/custom/path.bash
   source $HOME/.config/bash/custom/colors.bash
   source $HOME/.config/bash/share/git_status.bash
   source $HOME/.config/bash/commands/init.bash
