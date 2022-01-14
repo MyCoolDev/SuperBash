60b26bdf36f672c959f4957ba7e57488545e19c6
My_Cool <45515681+MyCoolDev@users.noreply.github.com>
2022-01-15 00:37:17 +0200

    Update path.bash

--- src/custom/path.bash
+++ src/custom/path.bash
@@ -1,11 +1,4 @@
-edit_path() {
-    localFiles=$1
-    icon=$2
-    # For exemple:
-    # if [[ $localFiles == "~/Development"* ]]; then
-    #   icon="<>"; localFiles="Dev"
-    # fi
-
-    echo "$localFiles $icon"
-    unset edit_path
-}
+# For exemple:
+# if [[ $localFiles == "~/Development"* ]]; then
+#   icon="<>"; localFiles="Dev"
+# fi
