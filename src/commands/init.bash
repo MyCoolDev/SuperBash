5aab4db49bd75725651a335ec6d32f339358595b
My_Cool <45515681+MyCoolDev@users.noreply.github.com>
2022-01-15 00:16:01 +0200

    Update init.bash

--- src/commands/init.bash
+++ src/commands/init.bash
@@ -1,3 +1,4 @@
 source $HOME/.config/bash/commands/dev.bash
 source $HOME/.config/bash/commands/config.bash
 source $HOME/.config/bash/commands/extra/package.bash
+source $HOME/.config/bash/commands/profile.bash
