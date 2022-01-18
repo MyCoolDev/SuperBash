3d545ff30be2ad9ae9ba3dc4c8efdf44275617e3
My_Cool <45515681+MyCoolDev@users.noreply.github.com>
2022-01-18 11:21:46 +0200

    Update init.bash

--- src/commands/init.bash
+++ src/commands/init.bash
@@ -1,4 +1,7 @@
 source $HOME/.config/bash/commands/dev.bash
 source $HOME/.config/bash/commands/config.bash
-source $HOME/.config/bash/commands/extra/package.bash
 source $HOME/.config/bash/commands/profile.bash
+
+# Extra Commands
+
+source $HOME/.config/bash/commands/extra/package.bash     # for arch linux
