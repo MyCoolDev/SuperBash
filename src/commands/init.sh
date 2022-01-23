61cd139082a26ba6d08d1e51b70956cae1d0d102
mycooldev <ron035994@gmail.com>
2022-01-23 23:09:38 +0200

    added extra commands filter

--- src/commands/init.sh
+++ src/commands/init.sh
@@ -3,5 +3,6 @@ source $HOME/.config/bash/commands/config.sh
 source $HOME/.config/bash/commands/profile.sh
 
 # Extra Commands
-
-source $HOME/.config/bash/commands/extra/package.sh         # for arch linux
+if [ -f /etc/arch-release ]; then
+    source $HOME/.config/bash/commands/extra/package.sh         # for arch linux
+fi
