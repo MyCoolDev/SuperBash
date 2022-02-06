9dfb5784493b847767940f5fbe3ed4c72e221e8c
kfir <kfirbotnik@gmail.com>
2022-02-06 09:26:29 +0200

    Update package.sh

--- src/commands/extra/package.sh
+++ src/commands/extra/package.sh
@@ -5,8 +5,8 @@ package() {
   elif [[ ! $package =~ ^[a-zA-Z0-9_\-]+$ ]]; then
     echo "package $package is invalid"
   else
-    respone=$(curl -I "https://aur.archlinux.org/packages/$package")
-    if [[ "$respone" == *"200"* ]]; then
+    respone=$(curl "https://aur.archlinux.org/packages/$package")
+    if [[ "$respone" != *"<title>AUR (en) - Not Found</title>"* ]]; then
       echo "Installing package $package..."
       start=${pwd}
       if [ ! -d "/tmp/packages" ]; then
