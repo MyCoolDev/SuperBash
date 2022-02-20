e6646f3c36192b07ed9cfe6e8c71e5632e64fdf3
mycooldev <ron035994@gmail.com>
2022-02-20 22:54:37 +0200

    update profile command!

--- src/commands/profile.sh
+++ src/commands/profile.sh
@@ -1,4 +1,8 @@
 profile() {
+    if ! [ -f "$HOME/.config/bash/profiles/$1.bash" ]; then
+        echo "Can't find profile!"
+        return
+    fi
     config=$(cat $HOME/.config/bash/custom/config.bdb)
     echo "PROFILE=$1;OSICON=$(bdbFind OSICON $config)" > $HOME/.config/bash/custom/config.bdb
     bash
